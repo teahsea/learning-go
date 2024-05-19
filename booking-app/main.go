17fa2ad29d16348acc17f2741faaadfba44eeb5e
Nikunj Prajapati <nikunjprajapati0016@gmail.com>
2024-05-19 22:33:57 +0530

    Simple logic building

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,9 +5,33 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = conferenceTickets
 
 	fmt.Println("Welcome to", conferenceName, "booking application")
 	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+
+	var userTickets uint
+
+	// ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName) // & means memory pointer - passing reference
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v ticktets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
